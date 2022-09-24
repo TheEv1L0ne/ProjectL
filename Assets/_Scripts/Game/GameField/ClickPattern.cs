5802492e0b562dd1d1fd87e38e7ea64569ef7039
Marko <marko.leskovsek.91@gmail.com>
2022-09-24 12:56:16 +0200

    Click patter class

---
+++ Assets/_Scripts/Game/GameField/ClickPattern.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Game.GameField
+{
+    public class ClickPattern
+    {
+        public int[,] clickPattern;
+        
+        public ClickPattern()
+        {
+            clickPattern = new[,]
+            {
+                {0, 1, 0}, 
+                {1, 1, 1}, 
+                {0, 1, 0}
+            };
+        }
+
+        public List<Vector2Int> ListOfCoordsOffset()
+        {
+            List<Vector2Int> listOfCoords = new List<Vector2Int>();
+            
+            int x = -1;
+            for (int i = 0; i < clickPattern.GetLength(0); i++)
+            {
+                int y = -1;
+                for (int j = 0; j < clickPattern.GetLength(1); j++)
+                {
+                    if (clickPattern[i, j] != 0)
+                    {
+                        var coords = new Vector2Int(x, y);
+                        listOfCoords.Add(coords);
+                    }
+
+                    y++;
+                }
+
+                x++;
+            }
+
+            return listOfCoords;
+        }
+    }
+}
