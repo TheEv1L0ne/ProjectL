b6e9854f9bc5aea516bc7cbb3ba6f97a42bbd898
Marko <marko.leskovsek.91@gmail.com>
2022-09-24 14:00:45 +0200

    Refactored ClickPattern class
    - pattern array is now readonly
    - added code comments

--- Assets/_Scripts/Game/GameField/ClickPattern.cs
+++ Assets/_Scripts/Game/GameField/ClickPattern.cs
@@ -6,11 +6,13 @@ namespace _Scripts.Game.GameField
 {
     public class ClickPattern
     {
-        public int[,] clickPattern;
+        private readonly int[,] _clickPattern;
         
         public ClickPattern()
         {
-            clickPattern = new[,]
+            //Using matrix like this only so it can be easily read.
+            //1 -> used for pattern shape
+            _clickPattern = new[,]
             {
                 {0, 1, 0}, 
                 {1, 1, 1}, 
@@ -18,20 +20,24 @@ namespace _Scripts.Game.GameField
             };
         }
 
+        /// <summary>
+        /// Creates offsets for coordinates that will be used to change game matrix states.
+        /// Since patterns are always 3x3 offset ranges are [-1,0,1] both for x and y.
+        /// </summary>
+        /// <returns>List of coordinate (x,y) offsets</returns>
         public List<Vector2Int> ListOfCoordsOffset()
         {
             List<Vector2Int> listOfCoords = new List<Vector2Int>();
             
             int x = -1;
-            for (int i = 0; i < clickPattern.GetLength(0); i++)
+            for (int i = 0; i < _clickPattern.GetLength(0); i++)
             {
                 int y = -1;
-                for (int j = 0; j < clickPattern.GetLength(1); j++)
+                for (int j = 0; j < _clickPattern.GetLength(1); j++)
                 {
-                    if (clickPattern[i, j] != 0)
+                    if (_clickPattern[i, j] != 0)
                     {
-                        var coords = new Vector2Int(x, y);
-                        listOfCoords.Add(coords);
+                        listOfCoords.Add(new Vector2Int(x, y));
                     }
 
                     y++;
