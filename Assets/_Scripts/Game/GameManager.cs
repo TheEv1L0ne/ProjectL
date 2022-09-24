0168abb0697c246ad7202424a0cb8828d98a793e
Marko <marko.leskovsek.91@gmail.com>
2022-09-24 12:57:17 +0200

    Added click logic to game field matrix

--- Assets/_Scripts/Game/GameManager.cs
+++ Assets/_Scripts/Game/GameManager.cs
@@ -1,17 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using _Scripts.Game.GameField;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
     [SerializeField] private GameField gameField;
 
+    private GameFieldMatrix _matrix;
+    private ClickPattern _clickPattern;
+
     private void Start()
     {
-        gameField.InitGameField((i, i1) =>
+        _matrix = new GameFieldMatrix();
+        _clickPattern = new ClickPattern();
+        
+        gameField.InitGameField((i, j) =>
         {
-            Debug.Log($"--->> [{i},{i1}] ");
+            var x = i;
+            var y = j;
+
+            var offsets = _clickPattern.ListOfCoordsOffset();
+            var listOfCoords = offsets.Select(offset => new Vector2Int(x + offset.x, y + offset.y)).ToList();
+            var listOfValidCoords = _matrix.GetValidFields(listOfCoords);
+            _matrix.SetNodeStates(listOfValidCoords);
+
+            foreach (var variablCoord in listOfValidCoords)
+            {
+                Debug.Log($"--->> [{variablCoord.x},{variablCoord.y}] ");
+            }
+
         });
     }
 }
